70b108ebae68e75968bf25e4d446df80a9cc2fde
YuChen <tony51527319527@gmail.com>
2026-01-13 17:31:28 +0800

    Import freedom spring project

---
+++ freedom/src/main/kotlin/com/seeddestiny/freedom/FreedomApplication.kt
@@ -0,0 +1,11 @@
+package com.seeddestiny.freedom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FreedomApplication
+
+fun main(args: Array<String>) {
+	runApplication<FreedomApplication>(*args)
+}
---
+++ freedom/src/test/kotlin/com/seeddestiny/freedom/FreedomApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.seeddestiny.freedom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FreedomApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
